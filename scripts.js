3b1cb56ab48a51c6a2b7ab65d1d6eb37cf241aaa
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2025-02-26 11:44:21 -0300

    finalização-javascripts

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 
 let chave = "b118836cc53220aebb82eaa23ff4ba77"
 
+function colocarNaTela(dados){
+    document.querySelector(".cidade").innerHTML = " Tempo em " + dados.name
+    document.querySelector(".tempo").innerHTML = Math.floor(dados.main.temp)  + " °C "
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".qualidade").innerHTML = " Umidade " + dados.main.humidity + "%"
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    console.log(dados)
+}
+
 async function buscarCidade(cidade){
     let dados = await fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" 
-        + cidade + "&appid=" + chave + "&lang=pt-br" + "&init"
+        + cidade + "&appid=" + chave + "&lang=pt_br" + "&units=metric"
     ).then( resposta => resposta.json())
-    console.log(dados)
+    
+    colocarNaTela(dados)
 }
 
 function cliqueinoBotao(){
